de5541bd4d18efcad0199c383b218976eaa31480
kanjearmstrong <47827157+kanjearmstrong@users.noreply.github.com>
2019-03-09 15:40:39 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Inclass62
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.d1 = new System.Windows.Forms.Label();
+            this.d2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Roll Dice";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(63, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 68);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(244, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 94);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(444, 91);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(110, 94);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // d1
+            // 
+            this.d1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.d1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.d1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.d1.Location = new System.Drawing.Point(274, 105);
+            this.d1.Name = "d1";
+            this.d1.Size = new System.Drawing.Size(80, 78);
+            this.d1.TabIndex = 4;
+            this.d1.Text = "0";
+            this.d1.Click += new System.EventHandler(this.d1_Click);
+            // 
+            // d2
+            // 
+            this.d2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.d2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.d2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.d2.Location = new System.Drawing.Point(469, 106);
+            this.d2.Name = "d2";
+            this.d2.Size = new System.Drawing.Size(85, 68);
+            this.d2.TabIndex = 5;
+            this.d2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 448);
+            this.Controls.Add(this.d2);
+            this.Controls.Add(this.d1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Roll Dice";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label d1;
+        private System.Windows.Forms.Label d2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inclass62
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Generates Dice ones Number
+            int dice1;
+
+            Random rand = new Random();
+
+
+            dice1 = rand.Next(6);
+
+            if (dice1 == 0)
+            {
+                d1.Text = "1";
+            }
+            else if (dice1 == 1)
+            {
+                d1.Text = "2";
+            }
+            else if (dice1 == 2)
+            {
+                d1.Text = "3";
+            }
+            else if (dice1 == 3)
+            {
+                d1.Text = "4";
+            }
+            else if (dice1 == 4)
+            {
+                d1.Text = "5";
+            }
+            else if (dice1 == 5)
+            {
+                d1.Text = "6";
+            }
+
+            //Generates Dice twos Numbers
+
+            int dice2 = rand.Next(6);
+            dice2 = rand.Next(6);
+            if (dice2 == 0)
+            {
+                d2.Text = "1";
+            }
+            else if (dice2 == 1)
+            {
+                d2.Text = "2";
+            }
+            else if (dice2 == 2)
+            {
+                d2.Text = "3";
+            }
+            else if (dice2 == 3)
+            {
+                d2.Text = "4";
+            }
+            else if (dice2 == 4)
+            {
+                d2.Text = "5";
+            }
+            else if (dice2 == 5)
+            {
+                d2.Text = "6";
+            }
+
+            //Counts amount of rolls and reads checks for snake eyes
+            counter++;
+
+
+            if (dice1 == 0 && dice2 == 0)
+            {
+                MessageBox.Show("It took " + counter + " rolls to get the snake eyes");
+                counter = 0;
+            }
+        }
+
+        private void picturebox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void d1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    }
+
